c9ed087d8bfa933b96263b5f049c1707a8c8af17
Esaul1391 <1391esaul@gmail.com>
2024-05-02 21:17:20 +0300

    start project

--- site3Dprint/test.py
+++ site3Dprint/test.py
@@ -1,8 +1,16 @@
-from django.test import TestCase
-from django.urls import reverse
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from django.test import LiveServerTestCase
 
 
-class BasicTest(TestCase):
-    def test_index(self):
-        response = self.client.get(reverse(''))  #
-        self.assertEqual(response.status_code, 200)
+class BasicInstallTest(LiveServerTestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_home_page(self):
+        self.browser.get(self.live_server_url)
+        self.assertIn("Сайт 3d печати", self.browser.title)

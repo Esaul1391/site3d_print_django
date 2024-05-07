22d1dea49b2e785e5d9862cf1602d373fc854209
roninnn1 <roninnn1@yandex.ru>
2024-05-07 17:09:03 +0300

    С рабоче

--- site3Dprint/printarticle/views.py
+++ site3Dprint/printarticle/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .models import Post
+from django.http import Http404
 
 
-def index(request):
-    pass
+def post_detail(request, id):
+    try:
+        post = Post.published.get(id=id)
+    except Post.DoesNotExist:
+        raise Http404('No Post found')
+    return render(request, 'printarticle/post/detail.html', {'post': post})
+
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request, 'printarticle/post/list.html', {'post': posts})

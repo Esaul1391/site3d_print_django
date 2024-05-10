feda2aa57a24f5ad67f3e70660cb0cf8cc031cfc
Esaul1391 <1391esaul@gmail.com>
2024-05-10 08:02:15 +0300

    add func taggit

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -11,7 +11,6 @@ from .forms import EmailPostForm, CommentForm
 def post_list(request, tag_slug=None):
     object_list = Post.published.all()
     tag = None
-
     if tag_slug:
         tag = get_object_or_404(Tag, slug=tag_slug)
         object_list = object_list.filter(tags__in=[tag])
